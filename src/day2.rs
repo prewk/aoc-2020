56fd4d3ef9c207a02474a933a8469f53a384921a
Oskar Thornblad <oskar.thornblad@gmail.com>
2020-12-20 15:59:17 +0100

    D2P2

--- src/day2.rs
+++ src/day2.rs
@@ -12,6 +12,14 @@ pub fn input_generator(input: &str) -> Vec<PasswordEntry> {
 pub fn part1(passwords: &[PasswordEntry]) -> u64 {
     passwords
         .iter()
-        .filter(|password| password.validate())
+        .filter(|password| password.sled_rental_validate())
+        .count() as u64
+}
+
+#[aoc(day2, part2)]
+pub fn part2(passwords: &[PasswordEntry]) -> u64 {
+    passwords
+        .iter()
+        .filter(|password| password.toboggan_validate())
         .count() as u64
 }
--- src/password.rs
+++ src/password.rs
@@ -1,8 +1,8 @@
 use regex::{Regex};
 
 pub struct PasswordEntry {
-    pub min: usize,
-    pub max: usize,
+    pub first: usize,
+    pub second: usize,
     pub char: char,
     pub password: String,
 }
@@ -18,20 +18,30 @@ impl PasswordEntry {
         let password = captures.get(4).unwrap().as_str().to_string();
 
         PasswordEntry {
-            min,
-            max,
+            first: min,
+            second: max,
             char,
             password
         }
     }
 
-    pub fn validate(&self) -> bool {
+    pub fn sled_rental_validate(&self) -> bool {
         let cnt = self.password
             .chars()
             .filter(|c| c == &self.char)
             .count();
 
-        cnt >= self.min  && cnt <= self.max
+        cnt >= self.first && cnt <= self.second
+    }
+
+    pub fn toboggan_validate(&self) -> bool {
+        let first_char = self.password.chars().nth(self.first - 1);
+        let second_char = self.password.chars().nth(self.second - 1);
+
+        let first_has = first_char.is_some() && first_char.unwrap() == self.char;
+        let second_has = second_char.is_some() && second_char.unwrap() == self.char;
+
+        first_has ^ second_has
     }
 }
 
@@ -43,20 +53,31 @@ mod tests {
     fn test_from_line() {
         let entry = PasswordEntry::from_line(&String::from("1-3 a: abcde"));
 
-        assert_eq!(entry.min, 1);
-        assert_eq!(entry.max, 3);
+        assert_eq!(entry.first, 1);
+        assert_eq!(entry.second, 3);
         assert_eq!(entry.char, 'a');
         assert_eq!(entry.password, "abcde".to_string());
     }
 
     #[test]
-    fn test_validate() {
+    fn test_sled_rental_validate() {
         let valid1 = PasswordEntry::from_line(&String::from("1-3 a: abcde"));
         let invalid1 = PasswordEntry::from_line(&String::from("1-3 a: aaaae"));
         let invalid2 = PasswordEntry::from_line(&String::from("1-3 a: xxxxx"));
 
-        assert!(valid1.validate());
-        assert!(!invalid1.validate());
-        assert!(!invalid2.validate());
+        assert!(valid1.sled_rental_validate());
+        assert!(!invalid1.sled_rental_validate());
+        assert!(!invalid2.sled_rental_validate());
+    }
+
+    #[test]
+    fn test_toboggan_validate() {
+        let valid1 = PasswordEntry::from_line(&String::from("1-3 a: abcde"));
+        let invalid1 = PasswordEntry::from_line(&String::from("1-3 b: cdefg"));
+        let invalid2 = PasswordEntry::from_line(&String::from("2-9 c: ccccccccc"));
+
+        assert!(valid1.toboggan_validate());
+        assert!(!invalid1.toboggan_validate());
+        assert!(!invalid2.toboggan_validate());
     }
 }
