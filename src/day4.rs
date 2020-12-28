06844754cd21ee510808571b93dab43416596162
Oskar Thornblad <oskar.thornblad@gmail.com>
2020-12-28 19:39:36 +0100

    D4P2

--- src/day4.rs
+++ src/day4.rs
@@ -4,7 +4,7 @@ use crate::passport::Passport;
 pub fn input_generator(input: &str) -> Vec<Passport> {
     input
         .split(&"\n\n")
-        .map(|chunk| Passport::from_line(&chunk.replace(&"\n", &" ")))
+        .map(|chunk| Passport::from_line(&chunk.replace(&"\n", &" ")).unwrap())
         .collect()
 }
 
@@ -13,9 +13,21 @@ pub fn part1(passports: &[Passport]) -> u64 {
     passports
         .iter()
         .fold(0, |acc, p| {
-            match p.validate() {
+            match p.validate_according_to_first_part() {
                 true => acc + 1,
                 false => acc,
+            }
+        })
+}
+
+#[aoc(day4, part2)]
+pub fn part2(passports: &[Passport]) -> u64 {
+    passports
+        .iter()
+        .fold(0, |acc, p| {
+            match p.validate_according_to_second_art() {
+                true => acc + 1,
+                false => acc
             }
         })
 }
--- src/passport.rs
+++ src/passport.rs
@@ -1,17 +1,174 @@
+use anyhow::{Result, bail};
+use regex::Regex;
+
+trait Validate {
+    fn validate(&self) -> bool;
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+pub struct Year {
+    pub value: u16,
+    min: u16,
+    max: u16
+}
+
+impl Year {
+    pub fn birth_year_from_str(year: &str) -> Result<Year> {
+        Ok(Year {
+            value: year.parse::<u16>()?,
+            min: 1920,
+            max: 2002,
+        })
+    }
+
+    pub fn issue_year_from_str(year: &str) -> Result<Year> {
+        Ok(Year {
+            value: year.parse::<u16>()?,
+            min: 2010,
+            max: 2020,
+        })
+    }
+
+    pub fn exp_year_from_str(year: &str) -> Result<Year> {
+        Ok(Year {
+            value: year.parse::<u16>()?,
+            min: 2020,
+            max: 2030,
+        })
+    }
+}
+
+impl Validate for Year {
+    fn validate(&self) -> bool {
+        self.value >= self.min && self.value <= self.max
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+pub enum Height {
+    Cm(u8),
+    Inch(u8),
+    Unknown,
+}
+
+impl Height {
+    pub fn from_str(height: &str) -> Result<Height> {
+        if height.ends_with(&"cm") {
+            Ok(Height::Cm(height[0..height.len() - 2].parse::<u8>()?))
+        } else if height.ends_with(&"in") {
+            Ok(Height::Inch(height[0..height.len() - 2].parse::<u8>()?))
+        } else {
+            Ok(Height::Unknown)
+        }
+    }
+}
+
+impl Validate for Height {
+    fn validate(&self) -> bool {
+        match self {
+            Height::Cm(v) => v >= &150u8 && v <= &193u8,
+            Height::Inch(v) => v >= &59u8 && v <= &76u8,
+            Height::Unknown => false,
+        }
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+pub struct HairColor {
+    color: String,
+}
+
+impl HairColor {
+    pub fn from_str(hair_color: &str) -> HairColor {
+        HairColor { color: hair_color.to_string() }
+    }
+}
+
+impl Validate for HairColor {
+    fn validate(&self) -> bool {
+        let re = Regex::new("^#[0-9a-f]{6}$").unwrap();
+
+        re.is_match(&self.color)
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+pub struct EyeColor {
+    color: String,
+}
+
+impl EyeColor {
+    pub fn from_str(color: &str) -> EyeColor {
+        EyeColor { color: color.to_string() }
+    }
+
+}
+
+impl Validate for EyeColor {
+    fn validate(&self) -> bool {
+        match &self.color.as_str() {
+            &"amb" => true,
+            &"blu" => true,
+            &"brn" => true,
+            &"gry" => true,
+            &"grn" => true,
+            &"hzl" => true,
+            &"oth" => true,
+            _ => false,
+        }
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+struct PassportId {
+    id: String,
+}
+
+impl PassportId {
+    pub fn from_str(id: &str) -> PassportId {
+        PassportId { id: id.to_string() }
+    }
+}
+
+impl Validate for PassportId {
+    fn validate(&self) -> bool {
+        let re = Regex::new("^\\d{9}$").unwrap();
+
+        re.is_match(&self.id)
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+struct CountryId {
+    id: String,
+}
+
+impl CountryId {
+    pub fn from_str(id: &str) -> CountryId {
+        CountryId { id: id.to_string() }
+    }
+}
+
+impl Validate for CountryId {
+    fn validate(&self) -> bool {
+        true
+    }
+}
+
 #[derive(PartialOrd, PartialEq, Debug)]
 pub struct Passport {
-    birth_year: Option<u16>,
-    issue_year: Option<u16>,
-    expiration_year: Option<u16>,
-    height: Option<String>,
-    hair_color: Option<String>,
-    eye_color: Option<String>,
-    passport_id: Option<String>,
-    country_id: Option<u16>,
+    birth_year: Option<Year>,
+    issue_year: Option<Year>,
+    expiration_year: Option<Year>,
+    height: Option<Height>,
+    hair_color: Option<HairColor>,
+    eye_color: Option<EyeColor>,
+    passport_id: Option<PassportId>,
+    country_id: Option<CountryId>,
 }
 
 impl Passport {
-    pub fn from_line(line: &str) -> Passport {
+    pub fn from_line(line: &str) -> Result<Passport> {
         let mut birth_year = None;
         let mut issue_year = None;
         let mut expiration_year = None;
@@ -21,25 +178,26 @@ impl Passport {
         let mut passport_id = None;
         let mut country_id = None;
 
-        line
-            .split(' ')
-            .for_each(|entry| {
-                let parts: Vec<&str> = entry.split(':').collect();
+        for entry in line.split(' ') {
+            let parts: Vec<&str> = entry.split(':').collect();
+            let part1 = parts.get(1).unwrap();
 
-                match parts.get(0) {
-                    Some(&"byr") => { birth_year = Some(parts.get(1).unwrap().parse::<u16>().unwrap()); },
-                    Some(&"iyr") => { issue_year = Some(parts.get(1).unwrap().parse::<u16>().unwrap()); },
-                    Some(&"eyr") => { expiration_year = Some(parts.get(1).unwrap().parse::<u16>().unwrap()); },
-                    Some(&"hgt") => { height = Some(parts.get(1).unwrap().to_string()); },
-                    Some(&"hcl") => { hair_color = Some(parts.get(1).unwrap().to_string()); },
-                    Some(&"ecl") => { eye_color = Some(parts.get(1).unwrap().to_string()); },
-                    Some(&"pid") => { passport_id = Some(parts.get(1).unwrap().to_string()); },
-                    Some(&"cid") => { country_id = Some(parts.get(1).unwrap().parse::<u16>().unwrap()); },
-                    _ => panic!("WTF"),
-                }
-            });
+            match parts.get(0) {
+                Some(&"byr") => { birth_year = Some(Year::birth_year_from_str(part1)?); },
+                Some(&"iyr") => { issue_year = Some(Year::issue_year_from_str(part1)?); },
+                Some(&"eyr") => { expiration_year = Some(Year::exp_year_from_str(part1)?); },
+                Some(&"hgt") => { height = Some(Height::from_str(part1)?); },
+                Some(&"hcl") => { hair_color = Some(HairColor::from_str(part1)); },
+                Some(&"ecl") => { eye_color = Some(EyeColor::from_str(part1)); },
+                Some(&"pid") => { passport_id = Some(PassportId::from_str(part1)); },
+                Some(&"cid") => { country_id = Some(CountryId::from_str(part1)); },
+                _ => {
+                    bail!("Invalid property: {}", part1);
+                },
+            }
+        }
 
-        Passport {
+        Ok(Passport {
             birth_year,
             issue_year,
             expiration_year,
@@ -48,10 +206,10 @@ impl Passport {
             eye_color,
             passport_id,
             country_id,
-        }
+        })
     }
 
-    pub fn validate(&self) -> bool {
+    pub fn validate_according_to_first_part(&self) -> bool {
         self.birth_year.is_some() &&
             self.issue_year.is_some() &&
             self.expiration_year.is_some() &&
@@ -59,7 +217,16 @@ impl Passport {
             self.hair_color.is_some() &&
             self.eye_color.is_some() &&
             self.passport_id.is_some()
+    }
 
+    pub fn validate_according_to_second_art(&self) -> bool {
+        self.birth_year.is_some() && self.birth_year.as_ref().unwrap().validate() &&
+            self.issue_year.is_some() && self.issue_year.as_ref().unwrap().validate() &&
+            self.expiration_year.is_some() && self.expiration_year.as_ref().unwrap().validate() &&
+            self.height.is_some() && self.height.as_ref().unwrap().validate() &&
+            self.hair_color.is_some() && self.hair_color.as_ref().unwrap().validate() &&
+            self.eye_color.is_some() && self.eye_color.as_ref().unwrap().validate() &&
+            self.passport_id.is_some() && self.passport_id.as_ref().unwrap().validate()
     }
 }
 
@@ -69,19 +236,19 @@ mod tests {
 
     #[test]
     fn test_passport_creation() {
-        let p = Passport::from_line(&"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm");
+        let p = Passport::from_line(&"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm").unwrap();
 
         assert_eq!(p, Passport {
-            birth_year: Some(1937),
-            issue_year: Some(2017),
-            expiration_year: Some(2020),
-            height: Some("183cm".to_string()),
-            hair_color: Some("#fffffd".to_string()),
-            eye_color: Some("gry".to_string()),
-            passport_id: Some("860033327".to_string()),
-            country_id: Some(147),
+            birth_year: Some(Year::birth_year_from_str(&"1937").unwrap()),
+            issue_year: Some(Year::issue_year_from_str(&"2017").unwrap()),
+            expiration_year: Some(Year::exp_year_from_str(&"2020").unwrap()),
+            height: Some(Height::from_str(&"183cm").unwrap()),
+            hair_color: Some(HairColor::from_str(&"#fffffd")),
+            eye_color: Some(EyeColor::from_str(&"gry")),
+            passport_id: Some(PassportId::from_str(&"860033327")),
+            country_id: Some(CountryId::from_str(&"147")),
         });
 
-        assert!(p.validate());
+        assert!(p.validate_according_to_first_part());
     }
 }
