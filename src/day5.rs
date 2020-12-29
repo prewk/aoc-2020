e6adb9f96791c66acaca3a1a7bc466a01d5bd7cc
Oskar Thornblad <oskar.thornblad@gmail.com>
2020-12-29 19:43:37 +0100

    D5P2

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,6 @@
 use crate::boarding::Seat;
+use std::collections::HashSet;
+use anyhow::{Result, bail};
 
 #[aoc_generator(day5)]
 pub fn input_generator(input: &str) -> Vec<Seat> {
@@ -15,4 +17,27 @@ pub fn part1(seats: &[Seat]) -> u64 {
         .map(|s| s.get_seat_id())
         .max()
         .unwrap()
+}
+
+#[aoc(day5, part2)]
+pub fn part2(seats: &[Seat]) -> Result<u64> {
+    let mut ids = HashSet::new();
+
+    for s in seats {
+        ids.insert(s.get_seat_id());
+    }
+
+    for id in &ids {
+        let behind = id - 1;
+        let before = id + 1;
+
+        if !ids.contains(&behind) && ids.contains(&(behind - 1)) {
+            return Ok(behind);
+        }
+        if !ids.contains(&before) && ids.contains(&(before + 1)) {
+            return Ok(before);
+        }
+    }
+
+    bail!("Oh noes");
 }
